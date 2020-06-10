a34f4f39765e78f25060c728e8ec4e8378bfe29d
Emanuele <ema.muna95@gmail.com>
2020-06-10 22:22:02 +0200

    Update datasetloader.py

--- datasetloader.py
+++ datasetloader.py
@@ -54,7 +54,7 @@ class ROD(VisionDataset):
             continue
           
           rgb_img_path = root+image_path.replace("???", "rgb").replace("***","crop")
-          depth_img_path = root+image_path.replace("???", "rgb").replace("***","depthcrop")
+          depth_img_path = root+image_path.replace("???", "surfnorm").replace("***","depthcrop")
           # Skip if one of the 2 (domains) images is not where it should be
           if not os.path.isfile(depth_img_path) or not os.path.isfile(rgb_img_path) :
               missing_couple += 1
@@ -62,7 +62,7 @@ class ROD(VisionDataset):
                 print("[INFO] Skipping 1 sample because of missing partner")
               continue
           rgb_img = pil_loader(rgb_img_path)
-          depth_img = pil_loader(rgb_img_path)
+          depth_img = pil_loader(depth_img_path)
           imgs_and_labels.append([rgb_img, depth_img, class_label])
           print("[INFO] A total of ", missing_couple, "samples were skipped because of a missing partner domain")
 
