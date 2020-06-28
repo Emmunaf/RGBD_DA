c8e1b128819c9ec7f687bd6618b28004ef56eb48
Emanuele <ema.muna95@gmail.com>
2020-06-28 11:42:47 +0200

    Added depth_to_gray_p to randomly convert some depth image to grayscale

--- jigvariation_datasetloader.py
+++ jigvariation_datasetloader.py
@@ -110,11 +110,13 @@ def jig_decode(enc_rot_label, n_rows):
      
 class ROD_patch(ROD):
 
-    def __init__(self, root, split='train', transform=None, target_transform=None, blacklisted_classes=[], verbose=0,n_samples=0, pre_rotation=False, min_width=0, min_height=0, n_rows=2, n_rand_rotate=2):
+    def __init__(self, root, split='train', transform=None, target_transform=None, blacklisted_classes=[], verbose=0,n_samples=0, pre_rotation=False, min_width=0, min_height=0, n_rows=2, n_rand_rotate=2, depth_to_gray_p=0.0):
         super(ROD_patch, self).__init__(root, split=split, transform=transform, target_transform=target_transform, blacklisted_classes=blacklisted_classes, verbose=verbose,n_samples=n_samples, pre_rotation=pre_rotation, min_width=min_width, min_height=min_height)
 
         # 
         self.n_rows, self.n_rand_rotate = n_rows, n_rand_rotate
+        self.depth_to_gray_p = depth_to_gray_p
+        
         assert(n_rand_rotate <= n_rows*n_rows)
 
         patch_label = []
@@ -157,8 +159,13 @@ class ROD_patch(ROD):
         rgb_image, depth_image, label, patch_rotations_label, enc_patch_rotations = self.data.iloc[index]['rgb'], self.data.iloc[index]['depth'], self.data.iloc[index]['encoded_class'], self.data.iloc[index]['patch_rotations_label'], self.data.iloc[index]['encoded_patch_rotations'] # Provide a way to access image and label via index
         
         patch_rotations = jig_decode(patch_rotations_label, self.n_rows)
-        #print(patch_rotations)
+        # Create the patch composed image
         t_depth_image =  patchize(depth_image, patch_rotations)
+        
+        # Apply gray transf if required
+        if self.depth_to_gray_p > 0.0 and self.depth_to_gray_p <= 1.0:
+            if make_decision(self.depth_to_gray_p):
+              t_depth_image = pil_to_gray(t_depth_image)
         # Applies preprocessing when accessing the image
         if self.transform is not None:
             rgb_image = self.transform(rgb_image)
@@ -174,18 +181,23 @@ class ROD_patch(ROD):
 
 class SynROD_patch(SynROD):
 
-    def __init__(self, root, transform=None, target_transform=None, blacklisted_classes=[], verbose=0, n_samples=None, pre_rotation=False, min_width=0, min_height=0, n_rows=2, n_rand_rotate=2):
+    def __init__(self, root, transform=None, target_transform=None, blacklisted_classes=[], verbose=0, n_samples=None, pre_rotation=False, min_width=0, min_height=0, n_rows=2, n_rand_rotate=2, depth_to_gray_p=0.0):
         
         """ Dataloader constructor.
         
         params:
         verbose: set a value >1 to have some useful infos.
         n_sample: Load a given number of sample with a random sapling (without replacement) tecnique. Reduce memory footprint.
+        n_rows: Number of rows. The dataloader will create a square n_rows*n_rows grid and rotate :n_rand_rotate patches from 0 to 270 degrees.
+        n_rand_rotate: Number of patches to be random rotated.
+        depth_to_gray_p: Probability to convert a depth rgb image to grayscale (mantaining rgb channels)
         """
         
         super(SynROD_patch, self).__init__(root, transform=transform, target_transform=target_transform, blacklisted_classes=blacklisted_classes, verbose=verbose,n_samples=n_samples, pre_rotation=pre_rotation, min_width=min_width, min_height=min_height)
 
         self.n_rows, self.n_rand_rotate = n_rows, n_rand_rotate
+        self.depth_to_gray_p = depth_to_gray_p
+        
         assert(n_rand_rotate <= n_rows*n_rows)
 
         patch_label = []
@@ -227,7 +239,12 @@ class SynROD_patch(SynROD):
 
         rgb_image, depth_image, label, patch_rotations_label, enc_patch_rotations = self.data.iloc[index]['rgb'], self.data.iloc[index]['depth'], self.data.iloc[index]['encoded_class'], self.data.iloc[index]['patch_rotations_label'], self.data.iloc[index]['encoded_patch_rotations'] # Provide a way to access image and label via index
         patch_rotations = jig_decode(patch_rotations_label, self.n_rows)
+        # Create patch composed image (with rotated patches)
         t_depth_image =  patchize(depth_image, patch_rotations)
+        # Apply gray transf if required
+        if self.depth_to_gray_p > 0.0 and self.depth_to_gray_p <= 1.0:
+            if make_decision(self.depth_to_gray_p):
+              t_depth_image = pil_to_gray(t_depth_image)
         # Applies preprocessing when accessing the image
         if self.transform is not None:
             rgb_image = self.transform(rgb_image)
